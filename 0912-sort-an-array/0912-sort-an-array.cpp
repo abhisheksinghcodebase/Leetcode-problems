641d11369e119e4fd94e1e74dd2415c6acc3ed14
Abhishek singh <abhisheksinghcode@zohomail.in>
2026-09-15 12:09:49 +0530

    Time: 24 ms (92.96%), Space: 73.1 MB (71.9%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int left, int mid, int right) {
-        vector<int> temp;
-
-        int i = left;
-        int j = mid + 1;
-
-        while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j]) {
-                temp.push_back(nums[i]);
-                i++;
-            } else {
-                temp.push_back(nums[j]);
-                j++;
-            }
-        }
-
-        while (i <= mid) {
-            temp.push_back(nums[i]);
-            i++;
-        }
-
-        while (j <= right) {
-            temp.push_back(nums[j]);
-            j++;
-        }
-
-        for (int k = 0; k < temp.size(); k++) {
-            nums[left + k] = temp[k];
-        }
-    }
-
-    void mergeSort(vector<int>& nums, int left, int right) {
+    void mergeSort(vector<int>& nums, vector<int>& temp, int left, int right) {
         if (left >= right)
             return;
 
         int mid = left + (right - left) / 2;
 
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
+        mergeSort(nums, temp, left, mid);
+        mergeSort(nums, temp, mid + 1, right);
 
-        merge(nums, left, mid, right);
+        // Already sorted, no need to merge
+        if (nums[mid] <= nums[mid + 1])
+            return;
+
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j])
+                temp[k++] = nums[i++];
+            else
+                temp[k++] = nums[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = nums[i++];
+
+        while (j <= right)
+            temp[k++] = nums[j++];
+
+        for (int x = left; x <= right; x++)
+            nums[x] = temp[x];
     }
 
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+        int n = nums.size();
+
+        vector<int> temp(n);
+
+        mergeSort(nums, temp, 0, n - 1);
+
         return nums;
     }
 };
