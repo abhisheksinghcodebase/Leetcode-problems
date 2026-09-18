6abd7047449eea7690baba3e646724adfc517a98
Abhishek singh <abhisheksinghcode@zohomail.in>
2026-09-18 12:13:06 +0530

    Time: 0 ms (100%), Space: 12 MB (12.77%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
@@ -1,15 +1,10 @@
-int* runningSum(int* nums, int numsSize, int* returnSize) {
-    int* result = (int*)malloc(numsSize * 4);
-
-    result[0] = nums[0];
-
-    int i = 1;
-    while(i<numsSize){
-        result[i] = result[i - 1] + nums[i];
-        i++;
-    }
-
+int * runningSum (int* nums, int numsSize, int* returnSize) {
     *returnSize = numsSize;
 
-    return result;
+    for (int i = 1; i < numsSize; i++)
+    {
+        nums[i] = nums[i] + nums[i - 1];
+    }
+
+    return nums;
 }
